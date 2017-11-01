14c952ecccbba167452aba4250f89ef88d3ad8f3
Andy Wang <andywang@eduroam-169-233-214-71.ucsc.edu>
2017-11-01 10:38:25 -0700

    updates to the adapter

--- main.c
+++ main.c
@@ -1,31 +1,28 @@
 /*
 
-Andy Wang
+  Andy Wang
 
-A command line front-end for libretro developed to study game-design
+  A front end driver for libretro
 
-Program Outline:
+  Program Outline TODO:
 
-  The server sends a byte as a "prompt" to say it's ready; let's say it sends 0.
-  The client sends a message like this:
-  Command ID, ...
-  Where the ... depends on the command.
-  Commands can be:
-  a. Step the emulator, collecting these informations, expecting this many players and this many inputs, and here are the inputs; (number of bytes per player input will be different for different consoles)
-  b. Save a state
-  c. Load a state, here it is
+    The server sends a byte as a "prompt" to say it's ready; let's say it sends 0.
+    The client sends a message like this:
+    Command ID, ...
+    Where the ... depends on the command.
+    Commands can be:
+    a. Step the emulator, collecting these informations, expecting this many players and this many inputs, and here are the inputs; (number of bytes per player input will be different for different consoles)
+    b. Save a state
+    c. Load a state, here it is
 
-Project Dependencies:
+  TODO:
+    Make scanRoms() and scanCores() flexible
 
-  http://glew.sourceforge.net/index.html for cross-platform OpenGL goodness
-  http://www.glfw.org/ for UI
+  Python extracting information.
 
-  FOR MAC USERS:
-    'brew install --build-bottle --static glfw3'
+  Impose a grid over the screen the of the game
 
-  http://www.portaudio.com for cross-platform audio
-
-FILE type is used in streams and is included in stdio.h
+  Knowing what parts of the screen is scrolling and what sprites are moving
 
 */
 
@@ -36,23 +33,63 @@ FILE type is used in streams and is included in stdio.h
 #include <dirent.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <dlfcn.h> //
 
-//External header files included - see top comment for info
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <portaudio.h>
 #include "libretro.h"
 
-//
-char* roms[100];
-static GLFWwindow* window; //
+/*
+Libretro API struct
+
+See libretro.h for more information on what the functions do
+*/
+static struct {
+
+  void *pointer; //pointer to the API
+
+  void (*retro_init)(void);
+
+  void (*retro_api_version)(void);
+
+  void (*retro_get_system_info)(struct retro_system_info *info);
+
+  void (*retro_unload_game)(void);
+
+  void (*retro_load_game)(const struct retro_game_info *game);
+
+  void (*retro_run)(void);
+
+  void (*retro_reset)(void);
+
+  void (*retro_deinit)(void);
+
+	bool ready; //is the API ready to go
+
+} api;
 
 /*
-scanDir()
-Looks in a directory and returns the files in it. (If any)
+Dynamically read
 */
-void scanDir(){
+#define link(V, S) do {\
+	if (!((*(void**)&V) = dlsym(api.pointer, #S))) \
+	   printf("API function failedddd '" #S "'': %s", dlerror()); \
+	} while (0)
+#define retro_link(S) link(api.S, S)
 
+//Arrays to hold a list of stuff in a directory
+char* roms[100];
+char* cores[10];
+
+//File pointer array
+FILE *romFile[100];
+FILE *coreFile[100];
+
+/*
+scanRoms()
+Looks in ROMS directory and returns the files in it. (If any)
+*/
+void scanRoms(){
+
+    //
     DIR *currDir;
     struct dirent *dir;
     struct stat statbuf;
@@ -81,6 +118,7 @@ void scanDir(){
          //Print name
          printf("[%d] %s \n", count, dir->d_name);
 
+         //
          count++;
 
       }
@@ -91,12 +129,110 @@ void scanDir(){
 }
 
 /*
-Starts a libretro core to run the ROM
-*/
-void init(){
+scanCores()
 
+Returns list of cores in a folder
+
+sidenote: connect the two functions
+*/
+
+void scanCores(){
+
+    DIR *currDir;
+    struct dirent *dir;
+    struct stat statbuf;
+
+    currDir = opendir("./cores");
+
+    //
+    int count = 0;
+
+    if (currDir){
+
+      while ((dir = readdir(currDir)) != NULL) {
+
+        //lstat() gets a link to the file
+        lstat(dir->d_name,&statbuf);
+
+        //If the files are any of these
+        if(strcmp(".",dir->d_name) == 0 || strcmp("..",dir->d_name) == 0 || strcmp(".DS_Store",dir->d_name) == 0){
+               //Nothing to print here, move onto next interation without finishing below
+               continue;
+         }
+
+         //Add the file to array
+         cores[count] = dir->d_name;
+
+         //Print name
+         printf("[%d] %s \n", count, dir->d_name);
+
+         count++;
+
+      }
+
+      closedir(currDir);
+    }
 
 }
+
+/*
+
+*/
+static void loadGame(const char *rom) {
+
+}
+
+/*
+
+*/
+static bool core_environment(unsigned cmd, void *data) {
+
+	return true;
+
+}
+
+
+/*
+
+*/
+static void loadCore(const char *sofile) {
+
+	void (*set_environment)(retro_environment_t) = NULL;
+
+	memset(&api, 0, sizeof(api));
+
+	api.pointer = dlopen(sofile, RTLD_LAZY);
+
+	if (!api.pointer){
+		//printf("Failed to load core: %s", dlerror());
+    printf("Failed to initialize libretro\n");
+
+	  dlerror();
+  }
+
+  //Access the required API function from the library
+  retro_link(retro_api_version);
+	retro_link(retro_init);
+	retro_link(retro_get_system_info);
+	retro_link(retro_reset);
+	retro_link(retro_load_game);
+	retro_link(retro_unload_game);
+	retro_link(retro_run);
+	retro_link(retro_deinit);
+
+
+	set_environment(core_environment);
+
+  //Libretro library initialize
+  api.retro_init();
+
+  //Core is ready
+	api.ready = true;
+
+	printf("Core loaded");
+
+}
+
 
 /*
 
@@ -119,7 +255,7 @@ int main(int argc, char* argv[]){
 
   printf("Now scanning the ROMS folder for possible games to launch.\n\n");
 
-  scanDir();
+  scanRoms(); //
 
   printf("\nPlease input the number correlated to your desired target ROM.\n\n");
 
@@ -128,58 +264,23 @@ int main(int argc, char* argv[]){
 
   printf("You selected %s\n\n", roms[atoi(num)]);
 
-  //
+  scanCores(); //
+
+  printf("Now select your core.\n\n");
+
+  //supports up to 100... if the ROM library is huge
+  fgets(num, 3, stdin);
+
+  printf("You selected %s\n\n", cores[atoi(num)]);
+
   printf("Initializing... \n\n");
 
-  init();
+  //PASS IN the dylib file
+  loadCore();// cores[atoi(num)] );
 
-  //
   printf("Ready!\n\n");
 
-/*
+	loadGame( roms[atoi(num)] );
 
-  //GLFW Example code below
-  if (!glfwInit()){
-    return -1;
-  }
-  */
-
-
-  /*
-  Create a windowed mode window and its OpenGL context
-  */
-  /*
-  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-  if (!window)
-  {
-      glfwTerminate();
-      return -1;
-  }
-  */
-
-  /* Make the window's context current */
-//  glfwMakeContextCurrent(window);
-
-  /* Loop until the user closes the window */
-
-  //while (!glfwWindowShouldClose(window)){
-      /* Render here */
-    //  glClear(GL_COLOR_BUFFER_BIT);
-
-      /* Swap front and back buffers */
-    //  glfwSwapBuffers(window);
-
-      /* Poll for and process events */
-    //  glfwPollEvents();
-  //}
-
-  //glfwTerminate();
-
-//  return 0;
-
-
-  //printf("%s\n", roms[0]);
-
-  //return (0);
 
 }
