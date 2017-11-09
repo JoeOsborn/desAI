c853c2a9189e947c4c812d5d8c7045a2457392ba
Andy Wang <andywang@eduroam-169-233-205-80.ucsc.edu>
2017-11-09 15:48:33 -0800

    Added pixel output

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
   Andy Wang
 
-  A front end driver for libretro
+  An adapter (front end) for libretro
 
   Program Outline TODO:
 
@@ -16,7 +16,8 @@
     c. Load a state, here it is
 
   TODO:
-    Make scanRoms() and scanCores() flexible
+
+  Make scanRoms() and scanCores() flexible
 
   Python extracting information.
 
@@ -36,6 +37,8 @@
 #include <dlfcn.h> //
 
 #include "libretro.h"
+#include "gl.h"
+//#include "jpg.h"
 
 /*
 Libretro API struct
@@ -58,9 +61,11 @@ static struct {
 
   void (*retro_get_system_info)(struct retro_system_info *info);
 
+  void (*retro_get_system_av_info)(struct retro_system_av_info *info);
+
   void (*retro_unload_game)(void);
 
-  void (*retro_load_game)(const struct retro_game_info *game);
+  bool (*retro_load_game)(const struct retro_game_info *game);
 
   void (*retro_run)(void);
 
@@ -71,6 +76,23 @@ static struct {
 	bool ready; //is the API ready to go
 
 } api;
+
+//OpenGL API Function links
+static struct{
+
+  //unsigned int GLenum;
+
+  //
+  void (*glBindTexture) (GLenum target, GLuint texture);
+
+  //
+  void (*glReadPixels)(GLint x, GLint y, GLsizei width, GLsizei height, GLenum format, GLenum type, GLvoid *pixels);
+
+} glFuncs;
+
+//void retro_camera_frame_raw(*retro_camera_frame_raw_framebuffer_t);
+
+//retroarch camera frame buffer callback
 
 /*
 Dynamically read
@@ -185,7 +207,7 @@ The section of the adapter to make environment callbacks
 See around line 450 of the libretro.h file
 
 */
-static bool core_environment(unsigned command, void *data) {
+bool core_environment(unsigned command, void *data) {
 
   switch(command){
 
@@ -202,7 +224,7 @@ static bool core_environment(unsigned command, void *data) {
 /*
 inputPoll()
 */
-static void inputPoll(){
+void inputPoll(){
 
   //Send inputs?
 
@@ -213,12 +235,118 @@ inputState()
 
 MATCH THIS :
 
-typedef int16_t (RETRO_CALLCONV *retro_input_state_t)(unsigned port, unsigned device,
-      unsigned index, unsigned id);
+  typedef int16_t (RETRO_CALLCONV *retro_input_state_t)(unsigned port, unsigned device,
+        unsigned index, unsigned id);
 
 */
-static int16_t inputState(unsigned port, unsigned device, unsigned index, unsigned id){
-  return 0;
+int16_t inputState(unsigned port, unsigned device, unsigned index, unsigned id){
+  return 1;
+}
+
+/*
+refreshVid()
+
+MATCHES :
+  typedef void (RETRO_CALLCONV *retro_video_refresh_t)(const void *data, unsigned width,
+        unsigned height, size_t pitch);
+
+"Pixel format is 15-bit 0RGB1555 native endian"
+
+The number of bytes between the beginning of a scanline and beginning of the next scanline is 512.
+
+*/
+void refreshVid(const void *data, unsigned width, unsigned height, size_t pitch){
+
+
+  //printf("%zu\n", pitch);
+
+  //printf("%d\n", width); 256
+
+  //printf("%d\n", height); 240
+
+
+   //glFuncs.glBindTexture(0x0DE1, glVars.texture);
+   //glFuncs.glBindTexture(GL_TEXTURE_2D, g_video.tex_id);
+
+   //stuff happens
+   if(data){
+
+      //FILE* frame = fopen("./images/01.jpg", "wb");
+
+      FILE* dump = fopen("./text/dump.txt", "wb");
+
+      fprintf(dump, "%s\n", data);
+
+      printf("data found\n");
+
+      //GLubyte *data = malloc(3*width*height);
+
+      //pitch = malloc(3*width*height);
+
+      //printf("%p\n",(void*)&data);
+
+      uint16_t *shorts = (uint16_t *)data;
+
+      fprintf(dump, "%hu\n", *shorts);
+
+      printf("------\n");
+
+      for(int i = 0; i < width*height; i+=1) {
+
+        //printf("%hu", shorts);
+
+
+        uint16_t pixel = shorts[i];
+        uint8_t red = 8*(pixel & (0b11111 << 10)) >> 10;
+        uint8_t green = 8*(pixel & (0b11111 << 5)) >> 5;
+        uint8_t blue = 8*(pixel & (0b11111));
+
+        //red, green, blue are between 0 and 2^6; scale them up to 0..2^8
+
+        printf("%hhu ",red);
+        printf("%hhu ",green);
+        printf("%hhu    ",blue);
+
+        //fill up another buffer with rgb data to use in opencv to dump
+
+        if (i%width==0){
+            printf("\n" );
+        }
+
+
+      }
+
+
+      //glFuncs.glReadPixels(0, 0, width, height, 0x1907 , 0x1401, data);
+      //glFuncs.glReadPixels(0, 0, width, height, GL_RGB , GL_UNSIGNED_BYTE, data);
+
+   }
+
+}
+
+
+
+/*
+audioSample()
+
+MATCH:
+  typedef void (RETRO_CALLCONV *retro_audio_sample_t)(int16_t left, int16_t right);
+*/
+void audioSample(int16_t left, int16_t right){
+  //audio happens
+
+}
+
+/*
+audioBatch()
+
+MATCH:
+  typedef size_t (RETRO_CALLCONV *retro_audio_sample_batch_t)(const int16_t *data,
+        size_t frames);
+*/
+size_t audioBatch(const int16_t *data, size_t frames){
+  //nice
+  return frames;
 }
 
 /*
@@ -229,14 +357,35 @@ Handles the necessary linking and callback
 print fuctions were for finding out when segmentation fault occurred.
 
 */
-static void loadCore(const char *sofile) {
+void loadCore(const char *sofile) {
+
+ /*
+
+ Have to link these, otherwise segmentation fault 11 during retro_run
+
+ RETRO_API void retro_set_environment(retro_environment_t);
+ RETRO_API void retro_set_video_refresh(retro_video_refresh_t);
+ RETRO_API void retro_set_audio_sample(retro_audio_sample_t);
+ RETRO_API void retro_set_audio_sample_batch(retro_audio_sample_batch_t);
+ RETRO_API void retro_set_input_poll(retro_input_poll_t);
+ RETRO_API void retro_set_input_state(retro_input_state_t);
+
+ */
 
  //Get things nice and ready
- void (*set_environment)(retro_environment_t) = NULL;
+ void (*retro_env)(retro_environment_t) = NULL;
 
- //input poll and state are a pair; otherwise seg fault 11
- void (*get_input)(retro_input_poll_t) = NULL;
- void (*get_state)(retro_input_state_t) = NULL;
+ //Input pair - poll and state
+ void (*retro_input)(retro_input_poll_t) = NULL;
+ void (*retro_inState)(retro_input_state_t) = NULL;
+
+ //Audio pair
+ void (*retro_audio)(retro_audio_sample_t) = NULL;
+ void (*retro_audio_batch)(retro_audio_sample_batch_t) = NULL;
+
+ //Visual
+ void (*retro_video)(retro_video_refresh_t) = NULL;
+ //void (*retro_frame)(retro_camera_frame_raw_framebuffer_t) = NULL;
 
   //printf("two\n" );
 
@@ -254,38 +403,59 @@ static void loadCore(const char *sofile) {
 	  dlerror();
   }
 
-  //
+  //yeh
   retro_link(retro_init);
 
-  //links: RETRO_API void retro_set_environment(retro_environment_t);
-  link(set_environment, retro_set_environment);
+  retro_link(retro_run);
 
-  printf("five\n" );
+  retro_link(retro_deinit);
+
+  retro_link(retro_load_game);
+
+  retro_link(retro_get_system_info);
+
+  retro_link(retro_get_system_av_info);
+
+  //links: RETRO_API void retro_set_environment(retro_environment_t);
+  link(retro_env, retro_set_environment);
 
   //links: RETRO_API void retro_set_input_poll(retro_input_poll_t);
-  link(get_input, retro_set_input_poll);
+  link(retro_input, retro_set_input_poll);
 
   //links: RETRO_API void retro_set_input_state(retro_input_state_t);
-  link(get_state, retro_set_input_state);
+  link(retro_inState, retro_set_input_state);
 
-  printf("six\n" );
+  //links: RETRO_API void retro_set_audio_sample(retro_audio_sample_t);
+  link(retro_audio, retro_set_audio_sample);
 
-	set_environment(core_environment);
+  //links: RETRO_API void retro_set_audio_sample_batch(retro_audio_sample_batch_t);
+  link(retro_audio_batch, retro_set_audio_sample_batch);
 
-  printf("seven\n" );
+  //links: RETRO_API void retro_set_video_refresh(retro_video_refresh_t);
+  link(retro_video, retro_set_video_refresh);
 
-  get_input(inputPoll);
+  //links:typedef void (RETRO_CALLCONV *retro_camera_frame_raw_framebuffer_t)(const uint32_t *buffer, unsigned width, unsigned height, size_t pitch);
+  //link(retro_frame, retro_camera_frame_raw);
 
-  printf("eight \n");
+  /*
+    Link the callbacks to functions existing in the main.c implementation
+  */
+	retro_env(core_environment);
 
-  get_state(inputState);
+  retro_input(inputPoll);
 
-  printf("nine \n");
+  retro_inState(inputState);
+
+  retro_audio(audioSample);
+
+  retro_audio_batch(audioBatch);
+
+  retro_video(refreshVid);
+
+  //retro_frame(frameBuffer);
 
   //Libretro library initialize
   api.retro_init();
-
-  printf("ten \n");
 
   //Core is ready
 	api.ready = true;
@@ -306,8 +476,11 @@ Define a struct for :
 */
 static void loadGame(const char *rom) {
 
+  struct retro_system_av_info avinfo = {0};
+  struct retro_system_info sys = {0};
+
   //Declaring the parameter the Libretro API wants
-  struct retro_game_info gameInfo;
+  struct retro_game_info gameInfo = { rom, 0 };
 
   //Read a non-text file, that is our ROM
   FILE *game = fopen(rom, "rb");
@@ -318,6 +491,24 @@ static void loadGame(const char *rom) {
   }
   else{
     printf("Are we rushin in, or are we going in sneaky beaky like? (Game file found) \n\n");
+
+    fseek(game, 0, SEEK_END);
+
+    gameInfo.size = ftell(game);
+
+    rewind(game);
+
+    api.retro_get_system_info(&sys);
+
+    gameInfo.data = malloc(gameInfo.size);
+
+    fread((void*)gameInfo.data, gameInfo.size, 1, game);
+
+    if ( !api.retro_load_game(&gameInfo)){
+        printf("dead on loading");
+    }
+
+    api.retro_get_system_av_info(&avinfo);
 
     //
     return;
@@ -336,6 +527,9 @@ Argv stores name of program at argv[0], and then proceeding indices are argument
 
 */
 int main(int argc, char* argv[]){
+
+  int count = 0;
+  int frames = 2000;
 
   char num[10];
 
@@ -366,7 +560,7 @@ int main(int argc, char* argv[]){
   //PASS IN the dylib file
   //printf("./cores/%s/libretro/quicknes_libretro.dylib", cores[atoi(num)]);
 
-  loadCore("./cores/quickNES/libretro/quicknes_libretro.dylib");
+  loadCore("./cores/bnes-libretro/bnes_libretro.dylib");
   //loadCore( cores[atoi(num)] );
 
   printf("Ready!\n\n");
@@ -374,10 +568,14 @@ int main(int argc, char* argv[]){
 	//loadGame( roms[atoi(num)] );
   loadGame("./ROMS/Super Mario Bros.nes"); //hard coding master
 
-  /*
-  segfault on retro_run... but why?
-    probably need to implement the rest of the set functions
-  */
-  api.retro_run();
+  //runs super mario for one frame
+  while (count < frames){
+    api.retro_run();
+    count++;
+  }
+
+  api.retro_deinit();
+
+  return 0;
 
 }
